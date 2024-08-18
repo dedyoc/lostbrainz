d0576f6333f87c1b44217f7d9277cf01d5712311
dedyoc <huyhoaius@gmail.com>
2024-08-18 13:36:49 +0700

    Quartz sync: Aug 18, 2024, 1:36 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          This lil garden's content is held together by duct tape, twigs and hope. Running on 
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
